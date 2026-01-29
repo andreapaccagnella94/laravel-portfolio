99c4fa78c98a7b915ea3f6f673104bbeb7875f4e
andreapaccagnella94 <andreapacca94@live.it>
2026-01-30 00:14:19 +0100

    Fix variable naming in projects index view for consistency

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -17,36 +17,36 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($projects as $progetto)
+            @foreach($projects as $project)
                 <tr>
-                    <td>{{ $progetto->name }}</td>
-                    <td>{{ $progetto->cliente }}</td>
-                    <td>{{ $progetto->periodo }}</td>
+                    <td>{{ $project->name }}</td>
+                    <td>{{ $project->cliente }}</td>
+                    <td>{{ $project->periodo }}</td>
                     <td class="d-flex justify-content-center gap-2 text-center">
-                        <a href="{{ route("projects.show", $progetto) }}" class="btn btn-outline-primary">
+                        <a href="{{ route("projects.show", $project) }}" class="btn btn-outline-primary">
                             Visualizza
                         </a>
-                        <a href="{{ route('projects.edit', $progetto) }}" class="btn btn-outline-warning">
+                        <a href="{{ route('projects.edit', $project) }}" class="btn btn-outline-warning">
                             Modifica
                         </a>
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#eliminaProgetto-{{ $progetto->id }}">
+                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#eliminaProgetto-{{ $project->id }}">
                             Elimina
                         </button>
                         <!-- Modal -->
-                        <div class="modal fade" id="eliminaProgetto-{{ $progetto->id }}" tabindex="-1" aria-labelledby="eliminaProgettoLabel-{{ $progetto->id }}" aria-hidden="true">
+                        <div class="modal fade" id="eliminaProgetto-{{ $project->id }}" tabindex="-1" aria-labelledby="eliminaProgettoLabel-{{ $project->id }}" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered"> {{-- mettere al centro il modale --}}
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="eliminaProgettoLabel-{{ $progetto->id }}">Elimina il Progetto: {{ $progetto->name }}</h1>
+                                        <h1 class="modal-title fs-5" id="eliminaProgettoLabel-{{ $project->id }}">Elimina il Progetto: {{ $project->name }}</h1>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        Vuoi eliminare il progetto "<strong>{{ $progetto->name }}</strong>"? Questa azione è definitiva.
+                                        Vuoi eliminare il progetto "<strong>{{ $project->name }}</strong>"? Questa azione è definitiva.
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                        <form action="{{ route('projects.destroy', $progetto->id) }}" method="POST">
+                                        <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
 
                                             @csrf
 
